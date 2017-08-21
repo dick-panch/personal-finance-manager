7783644201cead9d40bcddd80c1353f9768cd226
Dick Panch <dick.panch@gmail.com>
2017-08-21 22:16:57 +0530

    Change format of amount

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
+include ActionView::Helpers::NumberHelper
 module ApplicationHelper
 	def money_format(amount=nil, currency=nil)\
 		amount = 0.0 unless amount.present?
 		currency = '₹' if currency.nil?
-		"#{currency} #{amount}"
+		return "#{currency}#{number_with_delimiter(amount,delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/ )}"
 	end
 end
