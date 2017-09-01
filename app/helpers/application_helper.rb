19fa936f8cb58efafce70f9fd17ee18bcd43643c
Dick Panch <dick.panch@gmail.com>
2017-09-02 01:15:46 +0530

    Display balance

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
 	def money_format(amount=nil, currency=nil)\
 		amount = 0.0 unless amount.present?
 		currency = '₹' if currency.nil?
-		return "#{currency}#{number_with_delimiter(amount,delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/ )}"
+		if amount.negative?
+			return "#{currency}(#{number_with_delimiter(amount,delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/ )})"
+		else
+			return "#{currency}#{number_with_delimiter(amount,delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/ )}"
+		end
 	end
 end
