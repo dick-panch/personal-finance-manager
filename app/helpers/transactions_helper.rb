862254d769b167892ff55bf62069c6e5a18ede05
Dick Panch <dick.panch@gmail.com>
2017-09-03 01:20:47 +0530

    Optimize Code

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -7,4 +7,31 @@ module TransactionsHelper
       ['Custom', 'custom']
    	]
 	end
+
+	def list_month
+		[
+	    [Date::MONTHNAMES[1], 1],
+	    [Date::MONTHNAMES[2], 2],
+	    [Date::MONTHNAMES[3], 3],
+	    [Date::MONTHNAMES[4], 4],
+	    [Date::MONTHNAMES[5], 5],
+	    [Date::MONTHNAMES[6], 6],
+	    [Date::MONTHNAMES[7], 7],
+	    [Date::MONTHNAMES[8], 8],
+	    [Date::MONTHNAMES[9], 9],
+	    [Date::MONTHNAMES[10], 10],
+	    [Date::MONTHNAMES[11], 11],
+	    [Date::MONTHNAMES[12], 12],
+	  ]
+	end
+
+	def list_quarter
+		[
+      ['1st Quater', 1],
+      ['2nd Quater', 2],
+      ['3rd Quater', 3],
+      ['4th Quater', 4]
+    ]
+	end
+
 end
