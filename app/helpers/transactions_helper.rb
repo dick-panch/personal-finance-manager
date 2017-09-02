a9f501f62d9cd9e9c10efff4a1f26a0b3787f0f2
Dick Panch <dick.panch@gmail.com>
2017-09-03 01:16:07 +0530

    Optimize Code

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,10 @@
 module TransactionsHelper
+	def list_period_type
+		[
+			['Monthly', 'monthly'],
+      ['Quarterly', 'quarterly'],
+      ['Yearly', 'yearly'],
+      ['Custom', 'custom']
+   	]
+	end
 end
